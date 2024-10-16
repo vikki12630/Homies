e7be8f1df9ae59ad4ca0dab44d578cb017ed6017
vikas singh <vikas7490@gmail.com>
2024-10-16 18:36:31 +0530

    multer

---
+++ Homies_Backend/src/utils/cloudinary.ts
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+  api_key: process.env.CLOUDINARY_API_KEY as string,
+  api_secret: process.env.CLOUDINARY_API_SECRET as string,
+  secure: true,
+});
+
+// file upload
+const uploadOnCloudinary = async (localFilePath: string) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      folder: "HOMIES",
+      resource_type: "auto",
+      transformation: [{ quality: "auto" }, { fetch_format: "auto" }],
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
